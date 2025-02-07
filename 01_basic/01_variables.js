6c35539eeb527469e2d31c9414bf69ea9de13590
suman biswas <72162271+iconic-in@users.noreply.github.com>
2025-02-07 09:40:18 +0000

    #

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accoutId = 144785;
+let accountEmail = "bsuman497@gmail.com";
+var accountPassword = "12345";
+accountCity = "Bongaon";
+let accountState;
+
+//accoutId = 2345; // not allowed
+accountEmail = "iconic@gmail.com";
+accountPassword = "231321";
+accountCity = "subhaspally";
+console.log(accoutId);
+console.table([accountEmail,accoutId,accountPassword,accountCity,accountState]);
+
+/*  prefer not to use var because of issue in block scope  & function scope  */
