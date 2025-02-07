56d964557a5e95b28328a310a8b39a416d744137
suman biswas <72162271+iconic-in@users.noreply.github.com>
2025-02-07 10:45:34 +0000

    data types

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // trate all js as newer version
+
+
+console.log(3+3); 
+console.log(7+6);
+
+let name = "suman";
+let age = 20;
+let isLoggedIn = false;
+let state = null;
+
+// number => 2 to power 53
+// bignt = > 
+// string => '' /""
+// boolean => true / flase 
+// null => standlone value 
+// undefinded =>
+// symbol => unique
+
+
+// object 
+console.log(typeof null);
+console.log(typeof undefined);
