61b130d467847a70a31db2125f954898dd2b3ea2
suman biswas <72162271+iconic-in@users.noreply.github.com>
2025-03-05 07:13:38 +0000

    #operators

---
+++ 01_basic/operators.js
@@ -0,0 +1,74 @@
+// assingment opertors = assings values
+// addition + adds values 
+// multipictaion operator * multiply values
+// compersion operators >
+
+let x = 5;
+let y = 10;
+let z = x + y;
+console.log(z);
+
+let a = 6;
+let b = 7;
+let c = a * b;
+console.log(c);
+
+// types of javascript opeartors
+// arthimatic operators
+// assignment operators
+// string operators
+// logiacal operators
+// bitwise operators
+// ternary operators
+// type operatos
+
+let Number = 3;
+let sum = (100 + 5) * Number;
+console.log(sum);
+
+
+// + => addtion
+// - => subtrication
+// * => multification
+// ** => exponction
+// / => division
+// % => modlues
+// ++ => increment
+// -- => decrement
+
+// == equal operator
+// == equql value to equal type
+// != not eqeual 
+// !== not equal value or not equal type
+// > grater then
+// < less then
+// >= greater then or equel to
+// <= less then or equel to
+// ? ternary equel
+
+
+let text1 = "A"
+let text2 = "B"
+let result = text1 > text2
+console.log(result);
+
+let text3 = "20"
+let text4 = "50"
+let text5 = text3 > text4;
+console.log(text5);
+
+let fisrtName = "suman";
+let lastName = "Biswas"
+let fullnName = fisrtName + lastName;
+console.log(fullnName);
+
+let maleName = "Ankita"
+maleName +=  "ghosh";
+
+console.log(maleName);
+
+
+let p = 5 +  5;
+let q = "5" + 5;
+let r = "Hello" + 5;
+console.table([p,q,r]);
