0a7ba19dc1261799d0132ec5d0a0e7e74e150c8c
suman biswas <72162271+iconic-in@users.noreply.github.com>
2025-03-05 07:23:18 +0000

    new class

---
+++ 01_basic/function.js
@@ -0,0 +1,13 @@
+function Myfunction(p1, p2){
+    return p1 * p2;
+}
+
+let resultNumer = Myfunction(3,4);
+console.log(resultNumer);
+
+function tocelsiuse(fahrenheit){
+    return (5/9) * (fahrenheit - 32);
+}
+
+let value = tocelsiuse(77);
+console.log(value);
