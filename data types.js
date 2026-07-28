ce88a228f10af2f57daa78830d60150c30370583
suman biswas <72162271+iconic-in@users.noreply.github.com>
2026-07-28 19:25:05 +0000

    datatypes

---
+++ data types.js
@@ -0,0 +1,20 @@
+"use strict";
+
+alert("suman"); // we are using node js
+
+let name = "suman";
+let age = 28;
+let isLoggedIn = false;
+
+
+// number => 2 to power 53 ;
+// bignet 
+// string ="";
+// bollean => true or false ;
+// null => standlone value 
+// undefined => 
+// symbol =>
+// object 
+
+
+console.log(typeof "suman");
