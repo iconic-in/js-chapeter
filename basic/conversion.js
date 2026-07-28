ac3b15d8e4b2ba3615e0fa65acd1101526a0d9fb
suman biswas <72162271+iconic-in@users.noreply.github.com>
2026-07-28 19:38:13 +0000

    conversion

---
+++ basic/conversion.js
@@ -0,0 +1,12 @@
+let isLoggedIn = 1;
+console.log(typeof isLoggedIn);
+let score = 33;
+console.log(typeof score);
+let scores = "33"
+console.log(typeof scores);
+let valueInNumber = Numer(scores);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => nan
+//true => 1 , false => 0
