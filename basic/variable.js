af7852a1f75802c2229ed43580e975e2b83b0a98
suman biswas <72162271+iconic-in@users.noreply.github.com>
2026-07-28 19:05:51 +0000

    #29

---
+++ basic/variable.js
@@ -0,0 +1,12 @@
+const accountId = 743235;
+let accountEmail = "bsuman497@gmail.com";
+var accountPassword = '743235';
+accountCity = "Bongaon";
+console.log(accountId);
+accountEmail = "biswasgour97@gmail.com";
+accountPassword ="12345";
+let accountState;
+console.table([accountEmail, accountId, accountPassword,accountCity]);
+
+
+// not to use war bcz isssue @ function scope//
